4f20a9cbabcda7df3731ba2e10fc75b5e09e3d70
Teppei-tt <teppei.takata03@gmail.com>
2021-07-13 22:10:44 +0900

    ✨ pritter,ESlintの導入及び、必要なパッケージのインストール

---
+++ src/components/Sample.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Sample: React.FC = () => {
+  return (
+    <div>
+      <button
+        className="button"
+        onClick={() => {
+          console.log("button");
+        }}
+      >
+        ボタン
+      </button>
+    </div>
+  );
+};
+
+export default Sample;
